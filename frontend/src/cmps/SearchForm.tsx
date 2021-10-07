1104bd2e84c7e20c019ecbe0b475fc7c4614b890
Tal Ben David <talbd51@gmail.com>
2021-10-07 19:12:21 +0300

    tidying up

---
+++ frontend/src/cmps/SearchForm.tsx
@@ -0,0 +1,89 @@
+import React, { ChangeEvent, FormEvent } from 'react';
+
+// import Select from 'react-select';
+import CreatableSelect from 'react-select/creatable';
+import makeAnimated from 'react-select/animated';
+import { ActionMeta, OnChangeValue } from 'react-select';
+
+import { useState } from 'react';
+
+export const SearchForm = () => {
+  const animatedComponents = makeAnimated();
+
+  const options: Option[] = [
+    { value: 'music', label: 'Music' },
+    { value: 'food', label: 'Food' },
+    { value: 'video games', label: 'Video Games' },
+  ];
+  const [showTopics, setShowTopics] = useState(false);
+  const [topics, setTopics] = useState([]);
+
+  interface Option {
+    value: string;
+    label: string;
+  }
+
+  const handleChange = (
+    newValue: OnChangeValue<Option, true>,
+    actionMeta: ActionMeta<Option>
+  ) => {
+    console.log('new value:', newValue);
+    console.log(`action: ${actionMeta.action}`);
+    setTopics([...newValue]);
+  };
+  const search = (ev: FormEvent<HTMLInputElement>) => {
+    ev.preventDefault();
+    //if its topics and there are no topics, dont search
+    if (showTopics && !topics.length) {
+      console.log('Please pick at least one topic');
+      //need to add user message here
+      return;
+    }
+    console.log('topics:', topics);
+  };
+
+  // const setChatType = (ev: ChangeEvent<HTMLInputElement>): string => {
+  //   console.log('value:', ev.target.value);
+  //   if (ev.target.value === 'free-search') setShowTopics(false);
+  //   else setShowTopics(true);
+  //   return;
+  // };
+  return (
+    <div>
+      <form onSubmit={search}>
+        <div>
+          <input
+            name="chat-type"
+            id="free-search"
+            type="radio"
+            value="free-search"
+            onChange={() => setShowTopics(false)}
+            checked={!showTopics}
+          />
+          <label htmlFor="free-search"> Free Search</label>
+        </div>
+        <div>
+          <input
+            name="chat-type"
+            id="topics"
+            type="radio"
+            value="topics"
+            onChange={() => setShowTopics(true)}
+          />
+          <label htmlFor="topics"> Search people by topics</label>
+        </div>
+
+        {showTopics && (
+          <CreatableSelect
+            id="select-topic"
+            isMulti
+            options={options}
+            onChange={handleChange}
+            components={animatedComponents}
+          />
+        )}
+        <button type="submit">Search for a chat</button>
+      </form>
+    </div>
+  );
+};
--- frontend/src/pages/MainPage.jsx
+++ frontend/src/pages/MainPage.jsx
@@ -1,5 +1,10 @@
 import React from 'react';
-
+import { SearchForm } from '../cmps/SearchForm.tsx';
 export const MainPage = () => {
-  return <div>main page</div>;
+  return (
+    <section>
+      <h1>Free Chat</h1>
+      <SearchForm />
+    </section>
+  );
 };
