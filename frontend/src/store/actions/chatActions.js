f7496f8f8cb653fc6b8441804d2fc40c2999cfda
Tal Ben David <talbd51@gmail.com>
2021-10-25 14:35:54 +0300

    fixed addMsg condition to work even if there's just one user logged

--- frontend/src/store/actions/chatActions.js
+++ frontend/src/store/actions/chatActions.js
@@ -6,11 +6,19 @@ export const addMsg = (roomId, msg, uid, name) => {
     //maybe add away to edit msg.
     //that means giving the msg an id!
     return async dispatch => {
-        const currUser = await getLoggedinUser()
-        if (currUser._id !== uid) {
-            const room = await httpService.post(`room/chat/${roomId}`, { msg, uid, name })
-            const newMsg = room.msgs[room.msgs.length - 1]
-            dispatch({ type: 'ADD_MSG', newMsg })
+        try {
+            const currUser = await getLoggedinUser()
+            console.log(currUser);
+            if (currUser._id === uid) {
+                const room = await httpService.post(`room/chat/${roomId}`, { msg, uid, name })
+                const newMsg = room.msgs[room.msgs.length - 1]
+                dispatch({ type: 'ADD_MSG', newMsg })
+            }
+            else {
+                console.log('shouldnt happen. means the sender is not the logged in user(or guest)');
+            }
+        } catch (err) {
+            console.log('addMsg error:', err);
         }
     }
 }
