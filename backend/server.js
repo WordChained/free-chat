1db62f2fb42f24030e8c5ec0af774dc3128985fc
Tal Ben David <talbd51@gmail.com>
2021-10-27 16:15:46 +0300

    updated backend to fit the heroku backend

--- backend/server.js
+++ backend/server.js
@@ -11,6 +11,8 @@ const session = expressSession({
     cookie: { secure: false }
 })
 
+const publicPath = path.join(__dirname, 'build')
+
 const app = express()
 app.use(session)
 const server = http.createServer(app);
@@ -22,35 +24,35 @@ server.listen(port, () => {
     // console.log('Server is running on port: ' + port)
 })
 
-app.get('/api/setup-session', (req, res) => {
-    req.session.connectedAt = Date.now()
-    console.log('setup-session:', req.sessionID);
-    res.end()
-})
+
 
 app.use(express.json())
 
-app.use((err, req, res, next) => {
-    console.log(err);
-    if (err) {
-        return res.status(err.status).send({ "error": err.message });
-    }
-    next();
-});
+// app.use((err, req, res, next) => {
+//     console.log(err);
+//     if (err) {
+//         return res.status(err.status).send({ "error": err.message });
+//     }
+//     next();
+// });
 
-if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.resolve(__dirname, 'public')))
-    console.log(__dirname);
-} else {
-    const corsOptions = {
-        origin: ['http://127.0.0.1:8080', 'http://localhost:8080', 'http://localhost:3000',
-            'http://localhost:8081', 'http://127.0.0.1:3030', 'http://127.0.0.1:3000', 'http://localhost:3030',
-            'http://192.168.1.17:8080/', 'http://192.168.1.22:8080',
-        ],
-        credentials: true
-    }
-    app.use(cors(corsOptions))
-}
+console.log('publicPath:', publicPath);
+// if (process.env.NODE_ENV === 'production') {
+console.log('process.env.NODE_ENV:', process.env.NODE_ENV);
+app.use(express.static(path.resolve(__dirname, 'build')))
+// app.use(express.static(publicPath))
+console.log('__dirname:', __dirname);
+// } else {
+// const corsOptions = {
+//     origin: ['http://127.0.0.1:8080', 'http://localhost:8080', 'http://localhost:3000',
+//         'http://localhost:8081', 'http://127.0.0.1:3030', 'http://127.0.0.1:3000', 'http://localhost:3030',
+//         'http://192.168.1.17:8080/', 'http://192.168.1.22:8080',
+//     ],
+//     credentials: true
+// }
+// app.use(cors(corsOptions))
+// }
+
 const authRoutes = require('./api/auth/auth-routes')
 const userRoutes = require('./api/user/user-routes')
 const roomRoutes = require('./api/room/room-routes')
@@ -58,7 +60,21 @@ app.use('/api/auth', authRoutes)
 app.use('/api/user', userRoutes)
 app.use('/api/room', roomRoutes)
 
-
+// Make every server-side-route to match the index.html
+// so when requesting http://localhost:3030/index.html/car/123 it will still respond with
+// our SPA (single page app) (the index.html file) and allow vue/react-router to take it from there
+app.get('/*', (req, res) => {
+    res.sendFile(path.join(publicPath, 'index.html'))
+})
 const { socketService } = require('./services/socket-service')
 socketService(server, session)
-// const io = socketIo(server);
+
+// routes
+const setupAsyncLocalStorage = require('./middlewares/setupAls.middleware')
+app.all('*', setupAsyncLocalStorage)
+
+app.get('/api/setup-session', (req, res) => {
+    req.session.connectedAt = Date.now()
+    console.log('setup-session:', req.sessionID);
+    res.end()
+})
