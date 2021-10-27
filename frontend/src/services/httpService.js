5aa1c59bbc3c0f5b9b49cebe9b72b723cf514343
Tal Ben David <talbd51@gmail.com>
2021-10-27 16:50:19 +0300

    a

--- frontend/src/services/httpService.js
+++ frontend/src/services/httpService.js
@@ -2,7 +2,7 @@ import Axios from 'axios'
 // import { router } from '@/router'
 
 const BASE_URL = process.env.NODE_ENV === 'production'
-    ? '/api/'
+    ? 'https://free-chat-1.herokuapp.com/api/'
     : '//localhost:3030/api/'
 
 
